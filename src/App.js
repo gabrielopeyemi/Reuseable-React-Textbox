9d899337f6a401b772b167e190cf43ce5c382b29
gabrielopeyemi <famosipe2010@gmail.com>
2021-03-21 11:12:03 +0100

    almost

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Textbox from './Components/Textbox.jsx'
 function App() {
+
+  const getData =()=>{
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Textbox />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Textbox 
+          type='password'
+          
+        />
+        <div onClick={getData}>okay</div>
       </header>
     </div>
   );
--- src/Components/Textbox.jsx
+++ src/Components/Textbox.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import {
+    FaRegTimesCircle
+} from 'react-icons/fa'
+import {
+    AiOutlineEyeInvisible,
+    AiOutlineEye
+} from 'react-icons/ai';
+export default function Textbox(props) {
+    const { 
+        placeholder,
 
-export default function Textbox() {
+    } = props;
+    const [seen, setSeen] = React.useState(true)
+    const [type, setType] = React.useState(props.type)
+    const [hidden, setHidden] = React.useState(true)
+    const [value, setvalue] = React.useState(props.value)
+    const handleSeen =()=>{
+        setSeen(!seen)
+        if(seen) setType('password')
+        else setType('text')
+    }
+    React.useEffect(()=>{
+        if (type === 'text'){
+            setHidden(true)
+        }else{
+            setHidden(false)
+        }
+    },[]);
+    const handleChange =(e)=>{
+        const value = e.target.value;
+        setvalue(value)
+    }
     return (
         <div>
-            Box
+            <div className='text-box'>
+                <input 
+                    type={type} 
+                    class="form-control" 
+                    placeholder={placeholder}
+                    onChange={handleChange}
+                    value={value}
+                 />
+                <FaRegTimesCircle 
+                    onClick={()=> setvalue('')}
+                    style={{display : value ? 'block' : 'none', color: "#000"}}
+                />
+                <div style={{display: hidden ? 'none' : 'block'}} onClick={handleSeen}>
+                    {seen ? <AiOutlineEye style={{color: "#000"}}/> : <AiOutlineEyeInvisible  style={{color: "#000"}}/>}
+                </div>
+            </div>
         </div>
     )
 }
